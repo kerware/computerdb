91382f54ba1664ec82d7d024e155ee457e5384c3
kerware <olivier.charles@kerware.com>
2024-05-22 14:53:39 +0200

    Premier commit

---
+++ src/main/java/org/dev4test/computerdb/ComputerdbApplication.java
@@ -0,0 +1,13 @@
+package org.dev4test.computerdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerdbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dev4test/computerdb/ComputerdbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev4test.computerdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
